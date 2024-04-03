be28875e1426a6042f57f7bf92b8cbb37203cd3f
Bartłomiej Szałach <onegrx@gmail.com>
2024-04-03 13:18:40 +0200

    Comment Repo to run without Postgres in the container

--- lib/demo/application.ex
+++ lib/demo/application.ex
@@ -9,7 +9,8 @@ defmodule Demo.Application do
   def start(_type, _args) do
     children = [
       DemoWeb.Telemetry,
-      Demo.Repo,
+      # Comment Repo to run without Postgres in the container
+      # Demo.Repo,
       {DNSCluster, query: Application.get_env(:demo, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: Demo.PubSub},
       # Start the Finch HTTP client for sending emails
